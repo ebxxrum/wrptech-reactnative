fbbcb1cbab788a88b33202deba4970a312bcd4ba
eborum <ebxxrum@gmail.com>
2018-04-05 19:40:18 +0900

    add webview for rendering html

--- components/Report/index.js
+++ components/Report/index.js
@@ -1,28 +1,37 @@
 import React from 'react';
-import { View, Text, WebView, Dimensions, ScrollView, StyleSheet } from 'react-native';
-import { LinearGradient } from 'expo';
+import { ScrollView, WebView, View, Text, Dimensions, StyleSheet } from 'react-native';
 
 var { height, width} = Dimensions.get('window');
 
 const Report = props => {
   console.log("report!");
   return (
-    <ScrollView>
+    <ScrollView style={styles.container}>
       <View style={styles.contentWrapper}>
         <View style={styles.header}>
           <Text style={styles.title}>작업내용</Text>
         </View>
-        <View style={styles.body}>
-          <Text stlye={styles.text}>{props.report.work}</Text>
-        </View>
+        <WebView
+          source={{html: props.report.work}}
+          scalesPageToFit={false}
+          style={{
+            marginTop: 15,
+            height: 150,
+          }}
+        />
       </View>
       <View style={styles.contentWrapper}>
         <View style={styles.header}>
           <Text style={styles.title}>금주계획</Text>
         </View>
-        <View style={styles.body}>
-          <Text stlye={styles.text}>{props.report.plan}</Text>
-        </View>
+        <WebView
+          source={{html: props.report.plan}}
+          scalesPageToFit={false}
+          style={{
+            marginTop: 15,
+            height: 150,
+          }}
+        />
       </View>
     </ScrollView>
   );
@@ -30,6 +39,8 @@ const Report = props => {
 
 const styles = StyleSheet.create({
   contentWrapper: {
+    flex: 1,
+    flexDirection: 'column',
     padding: 15,
     borderBottomWidth: 1,
     borderBottomColor: 'rgba(0,0,0,0.05)',
@@ -45,8 +56,5 @@ const styles = StyleSheet.create({
     fontSize: 18,
     fontWeight: 'bold',
   },
-  body: {
-    paddingTop: 15,
-  }
 });
 export default Report;
