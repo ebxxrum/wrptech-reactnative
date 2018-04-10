275ac748303233ae8e01584ac9cc7b07b6138343
eborum <ebxxrum@gmail.com>
2018-04-10 15:56:21 +0900

    action button bug 수정

--- components/Action/index.js
+++ components/Action/index.js
@@ -1,57 +1,68 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { View, StyleSheet } from 'react-native';
 import { SimpleLineIcons } from '@expo/vector-icons';
 import ActionButton from 'react-native-action-button';
 
-const Action = props => {
-  console.log("action");
-  console.log(props);
-  return (
-    <View>
-      <ActionButton
-        buttonColor="rgba(223,47,60,100)"
-        btnOutRange="rgba(176,176,176,100)"
-        bgColor="rgba(0,0,0,0.85)"
-      >
-        <ActionButton.Item
-          textContainerStyle={styles.actionButtonTextContainer}
-          textStyle={styles.actionButtonText}
-          buttonColor='#fff'
-          title="마이페이지"
-          onPress={() => props.navigation.navigate('Profile')}
+class Action extends Component {
+  componentWillMount = () => {
+    const { profile, thisWeek } = this.props.screenProps;
+    thisWeek.map(thisWeek =>
+    profile.name === thisWeek.name && thisWeek.report &&
+      this.setState({
+        myReportIsNull: false,
+        myReport: thisWeek.report
+      })
+    )
+  };
+  
+  render() {
+    return (
+      <View>
+        <ActionButton
+          buttonColor="rgba(223,47,60,100)"
+          btnOutRange="rgba(176,176,176,100)"
+          bgColor="rgba(0,0,0,0.85)"
         >
-          <SimpleLineIcons name="user" style={styles.actionButtonIcon} />
-        </ActionButton.Item>
-        <ActionButton.Item
-          textContainerStyle={styles.actionButtonTextContainer}
-          textStyle={styles.actionButtonText}
-          buttonColor='#fff'
-          title="주별목록"
-          //TODO view: modal
-          onPress={() => {}}
-        >
-          <SimpleLineIcons name="calendar" style={styles.actionButtonIcon} />
-        </ActionButton.Item>
-        <ActionButton.Item
-          textContainerStyle={styles.actionButtonTextContainer}
-          textStyle={styles.actionButtonText}
-          buttonColor='#fff'
-          title="이번주 보고서"
-          onPress={() => props.navigation.navigate('Week')}
-        >
-          <SimpleLineIcons name="book-open" style={styles.actionButtonIcon} />
-        </ActionButton.Item>
-        <ActionButton.Item
-          textContainerStyle={styles.actionButtonTextContainer}
-          textStyle={styles.actionButtonText}
-          title={props.myReportIsNull ? "보고서 작성" : "보고서 수정"}
-          onPress={props.goForm}
-        >
-          <SimpleLineIcons name="pencil" style={[styles.actionButtonIcon, styles.mainButtonIcon]} />
-        </ActionButton.Item>
-      </ActionButton>
-    </View>
-  );
+          <ActionButton.Item
+            textContainerStyle={styles.actionButtonTextContainer}
+            textStyle={styles.actionButtonText}
+            buttonColor='#fff'
+            title="마이페이지"
+            onPress={() => this.props.navigation.navigate('Profile')}
+          >
+            <SimpleLineIcons name="user" style={styles.actionButtonIcon} />
+          </ActionButton.Item>
+          <ActionButton.Item
+            textContainerStyle={styles.actionButtonTextContainer}
+            textStyle={styles.actionButtonText}
+            buttonColor='#fff'
+            title="주별목록"
+            //TODO view: modal
+            onPress={() => {}}
+          >
+            <SimpleLineIcons name="calendar" style={styles.actionButtonIcon} />
+          </ActionButton.Item>
+          <ActionButton.Item
+            textContainerStyle={styles.actionButtonTextContainer}
+            textStyle={styles.actionButtonText}
+            buttonColor='#fff'
+            title="이번주 보고서"
+            onPress={() => this.props.navigation.navigate('Week')}
+          >
+            <SimpleLineIcons name="book-open" style={styles.actionButtonIcon} />
+          </ActionButton.Item>
+          <ActionButton.Item
+            textContainerStyle={styles.actionButtonTextContainer}
+            textStyle={styles.actionButtonText}
+            title={this.props.myReportIsNull ? "보고서 작성" : "보고서 수정"}
+            onPress={() => this.props.navigation.navigate('Form', {reportStatus: this.state.myReportIsNull, report: this.state.myReport})}
+          >
+            <SimpleLineIcons name="pencil" style={[styles.actionButtonIcon, styles.mainButtonIcon]} />
+          </ActionButton.Item>
+        </ActionButton>
+      </View>
+    );
+  }
 };
 
 const styles = StyleSheet.create({
