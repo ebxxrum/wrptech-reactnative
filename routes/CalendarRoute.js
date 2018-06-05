d46e893bb19bed482b6466eed122e27bc8ba053d
eborum <ebxxrum@gmail.com>
2018-06-05 13:55:51 +0900

    사용하지 않은 view 정리

--- routes/CalendarRoute.js
+++ routes/CalendarRoute.js
@@ -1,17 +1,10 @@
 import { StackNavigator } from 'react-navigation';
 import CalendarScreen from '../screens/CalendarScreen';
-import ReportList from '../components/ReportList';
 
 const CalendarRoute = StackNavigator(
   {
     Calendar: {
       screen: CalendarScreen,
-      navigationOptions: {
-        header: null
-      }
-    },
-    ReportList: {
-      screen: ReportList,
       navigationOptions: {
         header: null
       }
--- routes/WeekRoute.js
+++ routes/WeekRoute.js
@@ -1,18 +1,11 @@
 import { StackNavigator } from 'react-navigation';
 import WeekScreen from '../screens/WeekScreen';
-import Report from '../components/Report';
 import Form from '../components/Form';
 
 const WeekRoute = StackNavigator(
   {
     Week: {
       screen: WeekScreen,
-      navigationOptions: {
-        header: null
-      }
-    },
-    Report: {
-      screen: Report,
       navigationOptions: {
         header: null
       }
