d2561767532416ae4d23366440eb8ef767d42e45
eborum <ebxxrum@gmail.com>
2018-06-29 14:22:07 +0900

    add todo list in apiaxios

--- utils/apiAxios.js
+++ utils/apiAxios.js
@@ -1,14 +1,13 @@
 import axios from 'axios';
 
-// const API_URL = 'http://wr.promptech.co.kr/api'; 
 axios.defaults.baseURL = 'http://wr.promptech.co.kr/api'; 
 axios.defaults.headers['Access-Control-Allow-Origin'] = '*';
 axios.defaults.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE';
 axios.defaults.headers['Access-Control-Allow-Headers'] = 'Authorization';
 
+// TODO: make post api and replace callApi
 function callAxios(endpoint, accessToken) {
   // return axios(`/${endpoint}`, {
-
   return axios(`/${endpoint}?access_token=${accessToken}`, {
     // TODO: save access_token in header using FCM(firebase cloud managing)
   })
